c762681d53b62a0c87efe78663d2d3f3689dd611
Edwin Ludik <edwin.ludik@gmail.com>
2026-03-12 10:48:56 +0100

    fix dir

--- download_models.py
+++ download_models.py
@@ -12,8 +12,8 @@ print(f"Model files downloaded successfully: {nsfw_model_name}")
 
 ocr_model_name = "zai-org/GLM-OCR"
 ocr_dir = os.getenv("MODEL_PATH_OCR", "./model_ocr")
-if not os.path.exists(nsfw_dir):
-    os.makedirs(nsfw_dir)
+if not os.path.exists(ocr_dir):
+    os.makedirs(ocr_dir)
 print(f"Downloading model {ocr_model_name} to {ocr_dir}...")
 snapshot_download(repo_id=ocr_model_name, local_dir=ocr_dir)
 print(f"Model files downloaded successfully: {ocr_model_name}")
